da20b976cce1ad88655f130dcdb219b3d7278e00
Icha <footmouthkick@gmail.com>
2025-11-03 20:07:48 -0600

    Add copyright and license information to module.php

--- modules/Core/module.php
+++ modules/Core/module.php
@@ -1,5 +1,7 @@
 <?php
-
+// Astria - Personal Digital OS
+// Copyright (C) 2025 Icha Senpai
+// Licensed under AGPLv3. See LICENSE for details.
 return [
     'name'    => 'core',
     'version' => '0.0.1',
